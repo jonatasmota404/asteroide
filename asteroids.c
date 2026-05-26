7a80bef80814ba115395766fb1c1dd6356f3b70f
Jônatas Júnior <jonatasjr.019@gmail.com>
2026-05-26 15:54:55 -0300

    fazendo os tiros

--- asteroids.c
+++ asteroids.c
@@ -8,6 +8,7 @@
 #define MAX_ASTEROIDES 8
 #define PONTOS_ASTEROIDE 14
 #define PI 3.14159265358979f
+#define MAX_TIROS 4
 
 typedef struct 
 {
@@ -24,7 +25,18 @@ typedef struct {
     int ativo;
 } Asteroide;
 
+typedef struct
+{
+    float x,y;
+    float vx, vy;
+    int ativo;
+    int vida;
+    float angulo;
+} Tiro;
+
+
 Asteroide asteroides[MAX_ASTEROIDES];
+Tiro tiros[MAX_TIROS];
 
 void desenha_nave(SDL_Renderer* renderer, Nave* nave){
     int r_nariz = 20;
@@ -45,59 +57,56 @@ void desenha_nave(SDL_Renderer* renderer, Nave* nave){
 
 }
 
-void spawn_asteroid(Asteroide* asteroide){
+void spawn_asteroid(Asteroide* asteroides){
 
-    int borda = rand() % 4;
-    float rand_0_1 = (float)rand() / RAND_MAX;
-    float rand_1_0_1 = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-
-    float velocidade = 1.5f;
-
-    asteroide->ativo=1;
-    asteroide->raio = 10 + (40*rand_0_1);
-
-    for (int i = 0; i < PONTOS_ASTEROIDE; i++)
+    for (int i = 0; i < MAX_ASTEROIDES; i++)
     {
-        float variacao = 0.8f + ((float)rand() / RAND_MAX) * 0.4f;
-        asteroide->raios[i] = asteroide->raio * variacao;
+        int borda = rand() % 4;
+        float rand_0_1 = (float)rand() / RAND_MAX;
+        float rand_1_0_1 = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+
+        float velocidade = 1.5f;
+        asteroides[i].ativo=1;
+        asteroides[i].raio = 10 + (40*rand_0_1);
+
+        for (int j = 0; j < PONTOS_ASTEROIDE; j++)
+        {
+            float variacao = 0.8f + ((float)rand() / RAND_MAX) * 0.4f;
+            asteroides[i].raios[j] = asteroides[i].raio * variacao;
+        }
+        
+
+        switch (borda)
+        {
+        case 0:
+            asteroides[i].x = LARGURA * rand_0_1;
+            asteroides[i].y = -20;
+            asteroides[i].vy = velocidade * rand_0_1;
+            asteroides[i].vx = velocidade * rand_1_0_1;
+            break;
+        case 1:
+            asteroides[i].x = LARGURA * rand_0_1;
+            asteroides[i].y = ALTURA + 20;
+            asteroides[i].vx = velocidade * rand_1_0_1;
+            asteroides[i].vy = velocidade * rand_0_1 * -1;
+            break;
+        case 2:
+            asteroides[i].x = -20;
+            asteroides[i].y = ALTURA*rand_0_1;
+            asteroides[i].vy = velocidade * rand_1_0_1;
+            asteroides[i].vx = velocidade * rand_0_1;
+            break;
+        case 3:
+            asteroides[i].x = LARGURA + 20;
+            asteroides[i].y = ALTURA*rand_0_1;
+            asteroides[i].vy = velocidade * rand_1_0_1;
+            asteroides[i].vx = velocidade * rand_0_1*-1;
+            break;
+        
+        default:
+            break;
+        }
     }
-    
-
-
-    switch (borda)
-    {
-    case 0:
-        asteroide->x = LARGURA * rand_0_1;
-        asteroide->y = -20;
-        asteroide->vy = velocidade * rand_0_1;
-        asteroide->vx = velocidade * rand_1_0_1;
-        break;
-    case 1:
-        asteroide->x = LARGURA * rand_0_1;
-        asteroide->y = ALTURA + 20;
-        asteroide->vy = velocidade * rand_0_1 * -1;
-        asteroide->vx = velocidade * rand_1_0_1;
-        break;
-    case 2:
-        asteroide->x = -20;
-        asteroide->y = ALTURA*rand_0_1;
-        asteroide->vy = velocidade * rand_1_0_1;
-        asteroide->vx = velocidade * rand_0_1;
-        break;
-    case 3:
-        asteroide->x = LARGURA + 20;
-        asteroide->y = ALTURA*rand_0_1;
-        asteroide->vy = velocidade * rand_1_0_1;
-        asteroide->vx = velocidade * rand_0_1*-1;
-        break;
-    
-    default:
-        break;
-    }
-
-
-    
-
 
 }
 
@@ -139,6 +148,41 @@ void desenha_asteroides(SDL_Renderer* renderer, Asteroide* asteroides) {
     }
 }
 
+void inicia_tiros(Tiro* tiros){
+    for (int i = 0; i < MAX_TIROS; i++)
+    {
+        tiros[i].ativo = 0;
+    }
+}
+
+void atira(Nave* nave, Tiro* tiros){
+    for (int i = 0; i < MAX_TIROS; i++) {
+        tiros[i].angulo = nave->angulo;
+        if (!tiros[i].ativo) {
+            tiros[i].ativo = 1;
+            tiros[i].vida = 60;
+            tiros[i].vx = cos(nave->angulo) * 8.0f;
+            tiros[i].vy = sin(nave->angulo) * 8.0f;
+            tiros[i].x = nave->x + cos(nave->angulo) * 20;
+            tiros[i].y = nave->y + sin(nave->angulo) * 20;
+            break;
+        }
+    }
+}
+
+void desenha_tiro(SDL_Renderer* renderer, Tiro* tiros, Nave* nave){
+    for (int i = 0; i < MAX_TIROS; i++) {
+        if (tiros[i].ativo) {
+            tiros[i].x += tiros[i].vx;
+            tiros[i].y += tiros[i].vy;
+            tiros[i].vida--;
+            if (tiros[i].vida <= 0) tiros[i].ativo = 0;
+            SDL_RenderDrawLine(renderer, tiros[i].x, tiros[i].y, tiros[i].x + cos(tiros[i].angulo)*5, 
+            tiros[i].y + sin(tiros[i].angulo)*5);
+        }
+    }
+}
+
 int main() {
     
     srand(time(NULL));
@@ -156,11 +200,11 @@ int main() {
     nave.vx = 0;
     nave.vy = 0;
     nave.angulo = 0;
+    int cooldown_tiro = 0;
+    float velocidade = 0;
 
-    for (int i = 0; i < MAX_ASTEROIDES; i++)
-    {
-        spawn_asteroid(&asteroides[i]);
-    }
+    spawn_asteroid(asteroides);
+    inicia_tiros(tiros);
     
 
      while (rodando)
@@ -180,7 +224,7 @@ int main() {
             nave.vy += sin(nave.angulo) * 0.05;
         }
 
-        float velocidade = sqrt(nave.vx * nave.vx + nave.vy * nave.vy);
+        velocidade = sqrt(nave.vx * nave.vx + nave.vy * nave.vy);
         if (velocidade > 5.0f) {
             nave.vx = (nave.vx / velocidade) * 5.0f;
             nave.vy = (nave.vy / velocidade) * 5.0f;
@@ -192,13 +236,23 @@ int main() {
         nave.y += nave.vy;
         nave.x = fmod(nave.x + LARGURA, LARGURA);
         nave.y = fmod(nave.y + ALTURA, ALTURA);
+        
+        if (cooldown_tiro > 0) cooldown_tiro--;
 
+        if (cooldown_tiro > 0) cooldown_tiro--;
+
+        if (teclado[SDL_SCANCODE_SPACE] && cooldown_tiro == 0) {
+            atira(&nave, tiros);
+            cooldown_tiro = 15; // espera 15 frames (~0.25s) antes do próximo
+        }
+        
         
         SDL_SetRenderDrawColor(renderer, 10,10,30,255);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         desenha_nave(renderer, &nave);
         desenha_asteroides(renderer, asteroides);
+        desenha_tiro(renderer, tiros, &nave);
         SDL_RenderPresent(renderer);
 
         SDL_Delay(16); // 1000ms / 60fps ≈ 16ms por frame
